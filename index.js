489952f6c2d43f8e39593dd1e8b83efb734b5241
MVCRUST <max.crust7@gmail.com>
2025-11-28 10:38:35 +0000

    Updated the game to have a currently flawed scoring system

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
-//User number
-const prompt = require('prompt-sync')();
+let computerScore = 0;
+let userScore = 0;
+do {
+  //User number
+  const prompt = require("prompt-sync")();
 
-const userSelection = prompt('Please Select Rock, Paper, or Scissors:    ');
-console.log(`You chose ${userSelection}`);
+  const userSelection = prompt("Please Select Rock, Paper, or Scissors:    ");
+  console.log(`You chose ${userSelection}`);
 
+  // Computer number
+  const num = Math.floor(Math.random() * 3);
+  switch (num) {
+    case 0:
+      console.log("Rock");
+      break;
+    case 1:
+      console.log("Paper");
+      break;
+    case 2:
+      console.log("Scissors");
+      break;
+    default:
+      console.log("Please choose one of Rock, Paper, or Scissors");
+  }
 
-// Computer number
-const num = Math.floor((Math.random()*3));
-switch (num) {
-  case 0:
-    console.log("Rock");
-    break;
-  case 1:
-    console.log("Paper")
-    break;
-  case 2:
-    console.log("Scissors")
-    break;
-  default:
-    console.log("Please choose one of Rock, Paper, or Scissors");
-}
-
-if (userSelection == 'Rock' && num == 0) {
-console.log("Draw")
-} else if (userSelection == 'Rock' && num == 1) {
-    console.log('You lose')
-} else if (userSelection == 'Rock' && num == 2) {
-
-    console.log('You win')
-} else if (userSelection == 'Paper' && num ==0) {
-    console.log('You win')
-} else if (userSelection == 'Paper' && num == 1) {
-    console.log('You draw')
-} else if (userSelection == 'Paper' && num == 2) {
-    console.log('You lose')
-} else if (userSelection == 'Scissors' && num == 0) {
-    console.log('You lose')
-} else if (userSelection == 'Scissors' && num == 1) {
-    console.log('You win')
-} else if (userSelection == 'Scissors' && num == 2) {
-    console.log('You draw')
-}
+  if (userSelection == "Rock" && num == 0) {
+    console.log("Draw");
+  } else if (userSelection == "Rock" && num == 1) {
+    computerScore++;
+    console.log("You lose");
+  } else if (userSelection == "Rock" && num == 2) {
+    userScore++;
+    console.log("You win");
+  } else if (userSelection == "Paper" && num == 0) {
+    userScore++;
+    console.log("You win");
+  } else if (userSelection == "Paper" && num == 1) {
+    console.log("You draw");
+  } else if (userSelection == "Paper" && num == 2) {
+    computerScore++;
+    console.log("You lose");
+  } else if (userSelection == "Scissors" && num == 0) {
+    computerScore++;
+    console.log("You lose");
+  } else if (userSelection == "Scissors" && num == 1) {
+    userScore++;
+    console.log("You win");
+  } else if (userSelection == "Scissors" && num == 2) {
+    userScore++;
+    console.log("You draw");
+  }
+  console.log(
+    `You have: ${userScore} points, the computer has: ${computerScore} points.`
+  );
+} while (userScore < 3 || computerScore < 3){
+    return console.log("game over")
+};
